ac15f2961f576c8aa2b755158c29cf31ddc8b71e
Mark McLeod <markmcleod45@gmail.com>
2021-12-02 18:07:25 +0000

    Adding a new note updates the server. It doesn't display on the page yet

--- frontend/bundle.js
+++ frontend/bundle.js
@@ -2,7 +2,20 @@
   // index.js
   var inputElTitle = document.querySelector("#user_input_note_title");
   var inputElContent = document.querySelector("#user_input_note_content");
-  var buttonElSubmit = document.querySelector("#???");
-  buttonElSubmit.addEventListener("click", () => {
+  var buttonEl = document.querySelector("#add-note-button");
+  buttonEl.addEventListener("click", () => {
+    let userTitle = inputElTitle.value;
+    let userContent = inputElContent.value;
+    const data = {
+      title: userTitle,
+      content: userContent
+    };
+    fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then((response) => response.json());
   });
 })();
--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,24 @@
-const inputElTitle = document.querySelector('#user_input_note_title');
-const inputElContent = document.querySelector('#user_input_note_content');
-const buttonElSubmit = document.querySelector('#???');
+// Set variables for the html elements on index.html - the two input fields and the button
+let inputElTitle = document.querySelector('#user_input_note_title');
+let inputElContent = document.querySelector('#user_input_note_content');
+let buttonEl = document.querySelector('#add-note-button');
 
-buttonElSubmit.addEventListener('click', () => {
-  // let userInput = inputEl.value;
+// When the button is clicked, do this...
+buttonEl.addEventListener('click', () => {
+  let userTitle = inputElTitle.value; // set a variable for the title
+  let userContent = inputElContent.value; // set a variable for the content
+  // Create a data object with the title and content
+  const data = {
+    title: userTitle,
+    content: userContent
+  };
+  // Add the data object to the array in the server
+  fetch('http://localhost:3000/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
 });
