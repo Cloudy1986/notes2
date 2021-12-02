e3e8589fe92f8dba4b1882434ccfcc189ed964a1
Mark McLeod <markmcleod45@gmail.com>
2021-12-02 16:56:23 +0000

    PLast update before starting a new repo

--- frontend/bundle.js
+++ frontend/bundle.js
@@ -1,2 +1,8 @@
 (() => {
+  // index.js
+  var inputElTitle = document.querySelector("#user_input_note_title");
+  var inputElContent = document.querySelector("#user_input_note_content");
+  var buttonElSubmit = document.querySelector("#???");
+  buttonElSubmit.addEventListener("click", () => {
+  });
 })();
---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+const inputElTitle = document.querySelector('#user_input_note_title');
+const inputElContent = document.querySelector('#user_input_note_content');
+const buttonElSubmit = document.querySelector('#???');
+
+buttonElSubmit.addEventListener('click', () => {
+  // let userInput = inputEl.value;
+});
